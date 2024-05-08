5100bb885a0604e8c249f65000156f0d4a3963cd
Neveen Nabil <76603129+NevenNabil@users.noreply.github.com>
2024-05-08 21:58:21 +0300

    update

---
+++ src/components/data/Data.js
@@ -0,0 +1,289 @@
+export const nav = [
+  {
+    text: "home",
+    path: "/",
+  },
+  {
+    text: "about",
+    path: "/about",
+  },
+  {
+    text: "services",
+    path: "/services",
+  },
+  {
+    text: "blog",
+    path: "/blog",
+  },
+  {
+    text: "pricing",
+    path: "/pricing",
+  },
+  {
+    text: "contact",
+    path: "/contact",
+  },
+]
+export const featured = [
+  {
+    cover: "../images/hero/h1.png",
+    name: "wedding Planning",
+    total: "Your day deserves the best. We create fairy tale weddings that reflect your love Story.",
+  },
+  {
+    cover: "../images/hero/h2.png",
+    name: "Corporate Events",
+    total: "Impress your clients and colleagues with seamless corporate gatherings and conferences.",
+  },
+  {
+    cover: "../images/hero/h3.png",
+    name: "Birthday Parties",
+    total: "From first birthdays to milestone celebrations, we'll make every moment unforgettable.",
+  },
+  {
+    cover: "../images/hero/h4.png",
+    name: "Private Parties",
+    total: "Host exclusive gatherings that leave a lasting impression on your guests.",
+  },
+]
+export const list = [
+  {
+    id: 1,
+    cover: "../images/list/p-1.png",
+    name: "COXESBAZAR SEA BEACH CHAIR BOOKING",
+    location: "210 Zirak Road, Canada",
+    price: "$50",
+  },
+  {
+    id: 2,
+    cover: "../images/list/p-2.png",
+    name: "AMERICAN TOWMAN SHOWPLACE",
+    location: "5698 Zirak Road, NewYork",
+    price: "$70",
+  },
+  {
+    id: 3,
+    cover: "../images/list/p-7.png",
+    name: "SISTAHS IN BUSINESS EXPO 2024",
+    location: "5624 Mooker Market, USA",
+    price: "$80",
+  },
+  {
+    id: 4,
+    cover: "../images/list/p-4.png",
+    name: "TECH CAREER FAIR: EXCLUSIVE TECH HIRING EVENT",
+    location: "5621 Liverpool, London",
+    price: "$54",
+  },
+  {
+    id: 5,
+    cover: "../images/list/p-5.png",
+    name: "AUSTIN TECH CAREER FAIR",
+    location: "210 Montreal Road, Canada",
+    price: "$40",
+  },
+  {
+    id: 6,
+    cover: "../images/list/p-6.png",
+    name: "GREENWICH ECONOMIC FORUM",
+    location: "210 Zirak Road, Canada",
+    price: "$72",
+  },
+]
+export const awards = [
+  {
+    icon: <i class='fa-solid fa-trophy'></i>,
+    num: "32 M	",
+    name: "Blue Burmin Award",
+  },
+  {
+    icon: <i class='fa-solid fa-briefcase'></i>,
+    num: "43 M",
+    name: "Mimo X11 Award",
+  },
+  {
+    icon: <i class='fa-solid fa-lightbulb'></i>,
+    num: "51 M",
+    name: "Australian UGC Award",
+  },
+  {
+    icon: <i class='fa-solid fa-heart'></i>,
+    num: "42 M",
+    name: "IITCA Green Award",
+  },
+]
+export const location = [
+  {
+    id: 1,
+    name: "New Orleans, Louisiana",
+    Events: "10 Event",
+    cover: "./images/location/city-1.png",
+  },
+  {
+    id: 2,
+    name: "Jerrsy, United State",
+    Events: "20 Event",
+    cover: "./images/location/city-2.png",
+  },
+  {
+    id: 3,
+    name: "Liverpool, London",
+    Events: "14 Event",
+    cover: "./images/location/city-3.png",
+  },
+  {
+    id: 4,
+    name: "NewYork, United States",
+    Events: "30 Event",
+    cover: "./images/location/city-4.png",
+  },
+  {
+    id: 5,
+    name: "Montreal, Canada",
+    Events: "5 Event",
+    cover: "./images/location/city-5.png",
+  },
+  {
+    id: 6,
+    name: "California, USA",
+    Events: "15 Event",
+    cover: "./images/location/city-6.png",
+  },
+]
+export const team = [
+  {
+    list: "50",
+    cover: "../images/customer/team-1.jpg",
+    address: "Liverpool, Canada",
+    name: "Magic Johnson",
+    icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+  {
+    list: "70",
+    cover: "../images/customer/team-2.jpg",
+    address: "Montreal, Canada",
+    name: "Nick Vujicic",
+    icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+  {
+    list: "80",
+    cover: "../images/customer/team-3.jpg",
+    address: "Denever, USA",
+    name: "Oprah Winfrey",
+    icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+  {
+    list: "51",
+    cover: "../images/customer/team-4.jpg",
+    address: "2272 Briarwood Drive",
+    name: "Robin Sharma",
+    icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+  {
+    list: "42",
+    cover: "../images/customer/team-5.jpg",
+    address: "2272 Briarwood Drive",
+    name: "Tony Robbins",
+    icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+  {
+    list: "38",
+    cover: "../images/customer/team-5.jpg",
+    address: "Montreal, USA",
+    name: "Tony Robbins",
+    icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>]
+  },
+]
+export const price = [
+  {
+    plan: "Basic",
+    price: "29",
+    ptext: "per user, per month",
+    list: [
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "99.5% Uptime Guarantee",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "120GB CDN Bandwidth",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "5GB Cloud Storage",
+      },
+      { change: "color", icon: <i class='fa-solid fa-x'></i>, text: "Personal Help Support" },
+      { change: "color", icon: <i class='fa-solid fa-x'></i>, text: "Enterprise SLA" },
+    ],
+  },
+  {
+    best: "Best Value",
+    plan: "Standard",
+    price: "49",
+    ptext: "per user, per month",
+    list: [
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "99.5% Uptime Guarantee",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "150GB CDN Bandwidth",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "10GB Cloud Storage",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "Personal Help Support",
+      },
+      {
+        change: "color",
+        icon: <i class='fa-solid fa-x'></i>,
+        text: "Enterprise SLA",
+      },
+    ],
+  },
+  {
+    plan: "Platinum",
+    price: "79",
+    ptext: "2 user, per month",
+    list: [
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "100% Uptime Guarantee",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "200GB CDN Bandwidth",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "20GB Cloud Storage",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "Personal Help Support",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "Enterprise SLA",
+      },
+    ],
+  },
+]
+export const footer = [
+  {
+    title: "LAYOUTS",
+    text: [{ list: "Home Page" }, { list: "About Page" }, { list: "Service Page" }, { list: "Events Page" }, { list: "Contact Page" }, { list: "Single Blog" }],
+  },
+  {
+    title: "ALL SECTIONS",
+    text: [{ list: "Headers" }, { list: "Features" }, { list: "Attractive" }, { list: "Testimonials" }, { list: "Videos" }, { list: "Footers" }],
+  },
+  {
+    title: "COMPANY",
+    text: [{ list: "About" }, { list: "Blog" }, { list: "Pricing" }, { list: "Affiliate" }, { list: "Login" }, { list: "Changelog" }],
+  },
+]
